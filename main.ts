62d6d3726f0ddf71dd935d0910e98e07fab889b8
Wren <123883708+turnipsforme@users.noreply.github.com>
2026-09-06 19:46:35 +0100

    Remove redundant settings heading

--- main.ts
+++ main.ts
@@ -423,7 +423,6 @@ class LetterboxdSyncSettingTab extends PluginSettingTab {
     const { containerEl } = this;
     containerEl.empty();
 
-    new Setting(containerEl).setName("Settings").setHeading();
     containerEl.createEl("p", {
       text: "RSS only includes recent diary items. Keep plugin enabled for continuous history.",
     });
